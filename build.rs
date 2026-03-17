3a4b0976f422809bb3e879bfde3438e2c71c9d5a
Konstantinos Rokanas <rokanas@student.chalmers.se>
2026-03-17 22:13:27 +0200

    #9 Add native windows compilation (for pipeline build)

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
-// use windres to embed icon in exe on windows
 fn main() {
+    // use different windres for windows vs cross-compilation
+    let windres = if cfg!(target_os = "windows") {
+        "windres"
+    } else {
+        "x86_64-w64-mingw32-windres"
+    };
+
+    // use windres to embed icon in exe on windows
     // place icon.res in cargo temp build directory instead of source foolder
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let res_path = format!("{}/icon.res", out_dir);
 
-    std::process::Command::new("x86_64-w64-mingw32-windres")
+    std::process::Command::new(windres)
         .args(["assets/icon.rc", "-O", "coff", "-o", &res_path])
         .status()
         .unwrap();
