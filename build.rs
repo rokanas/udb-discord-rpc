d2d769ddb557856325c0dab760bc523d54704a15
Konstantinos Rokanas <rokanas@student.chalmers.se>
2026-03-17 15:34:31 +0200

    #5 Add build.rs and winres call to embed icon

---
+++ build.rs
@@ -0,0 +1,9 @@
+// use winres to embed icon in exe on windows
+fn main() {
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
